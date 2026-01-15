4f34858d93d40f99adf6fac3513c782c6ff94c0f
Ranco.nyc <ranco.nyc@gmail.com>
2026-01-14 17:53:50 -0800

    fix: signin error 7

--- src/app/profile/[id]/types.ts
+++ src/app/profile/[id]/types.ts
@@ -1,4 +1,32 @@
-import { User, UserProfile, UserMedia } from "@prisma/client";
+// import { User, UserProfile, UserMedia } from "@prisma/client";
+import { City } from "@/domain/city/city.schema";
+import { User, UserMedia } from "@prisma/client";
+
+export interface UserProfile {
+  id: string;
+  userId: string;
+  user: User; // Ensure 'User' is defined elsewhere
+
+  firstName: string;
+  lastName: string;
+  occupation: string;
+  birthday: string;
+  description: string;
+  gender: string;
+
+  languages: string[];
+
+  homeBaseCityId: string;
+  homeBaseCity: City;
+
+  persona: {
+    dailyRhythm: string;
+    travelStyle: string;
+    interest: { [key: string]: string[] };
+  };
+
+  socials: { [key: string]: string }; // Map of social platform names to URLs
+}
 
 export type ProfileUser = User & {
   profile:
