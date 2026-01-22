4e55f52c8c2a48bd678c207d231a7961ea3aea6b
Ranco.nyc <ranco.nyc@gmail.com>
2026-01-21 22:46:06 -0800

    fix: Compatibility breakdown build error

--- src/app/profile/[id]/components/modal/MatchAvatar.tsx
+++ src/app/profile/[id]/components/modal/MatchAvatar.tsx
@@ -13,11 +13,15 @@ export default function MatchAvatar({
   return (
     <div className="relative flex -space-x-4 justify-center mb-4">
       <Avatar
-        image={profileUser.avatarUrl}
-        name={profileUser.name}
+        image={profileUser.avatarUrl ?? undefined}
+        name={profileUser.name ?? ""}
         size={112}
       />
-      <Avatar image={loggedUser.avatarUrl} name={loggedUser.name} size={112} />
+      <Avatar
+        image={loggedUser.avatarUrl ?? undefined}
+        name={loggedUser.name ?? ""}
+        size={112}
+      />
       <div className="absolute -bottom-2 left-1/2 -translate-x-1/2 flex bg-brand text-white rounded-full">
         <div className="w-14 h-14 rounded-full flex flex-col -space-y-1 items-center justify-center">
           <div className="text-[10px] font-bold uppercase">match</div>
